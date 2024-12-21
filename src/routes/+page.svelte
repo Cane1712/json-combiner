dcb3ffbb15213caa4e9eb6f63c7874582b27dccc
Cane1712 <cane17124@gmail.com>
2024-12-21 17:50:56 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let input = $state('{"someFlag":"someValue"}');
+    let json = $derived(JSON.parse(input) ?? {})
+    let combined = $state({});
+
+    function onclick() {
+        combined = {...combined, ...json}
+        input = "{}"
+    }
+</script>
+
+<h1 class="text-2xl flex justify-center mt-8">JSON combiner</h1>
+<div class="grid grid-cols-1 mx-auto justify-between mt-8 justify-items-center">
+    <div class="columns-1">
+        <h1 class="text-xl mx-auto">Load JSON</h1><br>
+        <input bind:value={input}  type="text"><br><br>
+        <button {onclick} class=" bg-blue-100">Add to selection</button> 
+        <button class="bg-blue-100">Copy JSON</button>
+    </div>
+    <br>
+</div>
