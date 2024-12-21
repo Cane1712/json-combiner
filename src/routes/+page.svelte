5f0321ef35c4d69e355b1fadedbda278f11adfce
Cane1712 <cane17124@gmail.com>
2024-12-21 21:05:39 +0100

    fix: use textarea instead of input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,23 @@
 </script>
 
 <h1 class="text-2xl flex justify-center mt-8">JSON combiner</h1>
-<div class="grid grid-cols-1 mx-auto justify-between mt-8 justify-items-center">
-    <div class="columns-1">
-        <h1 class="text-xl mx-auto">Load JSON</h1><br>
-        <input bind:value={input}  type="text"><br><br>
-        <button {onclick} class=" bg-blue-100">Add to selection</button> 
+<div class="grid grid-cols-1 mx-auto grid-col mt-8 justify-items-center">
+    <div class="mb-8">
+        <button {onclick} class=" bg-blue-100">Combine JSON</button><br> <br>
         <button onclick={async (e) => {await navigator.clipboard.writeText(JSON.stringify(combined))}} class="bg-blue-100">Copy JSON</button>
+    </div>
+    <div>
+        <!-- <h1 class="text-xl mx-auto">Load JSON</h1><br> -->
+        <textarea bind:value={input} class="w h"> </textarea><br><br>
     </div>
     <br>
 </div>
+
+<style>
+    .w {
+        width: 30rem
+    } 
+    .h {
+        height: 12em;
+    }
+</style>
