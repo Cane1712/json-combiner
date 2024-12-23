6ff28c87933246bf8b35c5144858c138a1d8dbd4
Cane1712 <cane17124@gmail.com>
2024-12-23 21:37:21 +0100

    feat: attempt json validation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     let input = $state('{"someFlag":"someValue"}');
+    let button: HTMLButtonElement;
     let json = $derived.by(() => {
         try {
             return JSON.parse(input) ?? {};
@@ -14,6 +15,11 @@
         input = "{}"
     }
 
+    async function validateJSON() {
+        const allflags = await fetch("https://clientsettings.roblox.com/v2/settings/application/PCDesktopClient").then(res => res.json());
+        button.innerHTML = `Validate JSON (${Object.keys(combined).filter(key => allflags[key] == undefined).length === 0})`
+        // return Object.keys(combined).filter(key => allflags[key] == undefined).length === 0;
+    }
 </script>
 
 <h1 class="text-2xl flex justify-center mt-8">JSON combiner</h1>
@@ -21,6 +27,7 @@
     <div class="mb-8">
         <button {onclick} class=" bg-blue-100 w-40">Combine JSON</button><br> <br>
         <button onclick={async (e) => {await navigator.clipboard.writeText(JSON.stringify(combined))}} class="bg-blue-100 w-40">Copy JSON</button>
+        <button bind:this={button} onclick={async (e) => await validateJSON()}>Validate JSON</button>
     </div>
     <div>
         <!-- <h1 class="text-xl mx-auto">Load JSON</h1><br> -->
